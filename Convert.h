ba230b05183b91988b83bd55d0ede7f9ffc4eada
doyaGu <10153309+doyaGu@users.noreply.github.com>
2024-03-04 11:34:43 -0500

    Fix VxConvertMatrix

--- Convert.h
+++ Convert.h
@@ -53,51 +53,49 @@ inline void VxConvertQuaternion(const VxQuaternion &in, IVP_U_Quat &out)
 
 inline void VxConvertMatrix(const IVP_U_Matrix &in, VxMatrix &out)
 {
-    out[0][0] = (float)in.get_elem(0, 0);
-    out[0][1] = (float)in.get_elem(0, 1);
-    out[0][2] = (float)in.get_elem(0, 2);
-    out[0][3] = (float)in.vv.k[0];
+    // Transpose
+    for (int i = 3; i >= 0; --i)
+        for (int j = 3; j >= 0; --j)
+            out[j][i] = (float)in.get_elem(i, j);
+    out[0][3] = 0.0f;
+    out[1][3] = 0.0f;
+    out[2][3] = 0.0f;
 
-    out[1][0] = (float)in.get_elem(1, 0);
-    out[1][1] = (float)in.get_elem(1, 1);
-    out[1][2] = (float)in.get_elem(1, 2);
-    out[1][3] = (float)in.vv.k[1];
-
-    out[2][0] = (float)in.get_elem(2, 0);
-    out[2][1] = (float)in.get_elem(2, 1);
-    out[2][2] = (float)in.get_elem(2, 2);
-    out[2][3] = (float)in.vv.k[2];
-
-    out[3][0] = out[3][1] = out[3][2] = 0.0f;
+    out[3][0] = (float)in.vv.k[0];
+    out[3][1] = (float)in.vv.k[1];
+    out[3][2] = (float)in.vv.k[2];
     out[3][3] = 1.0f;
 }
 
 inline void VxConvertMatrix(const VxMatrix &in, IVP_U_Matrix &out)
 {
+    // Transpose
     out.set_elem(0, 0, in[0][0]);
-    out.set_elem(0, 1, in[0][1]);
-    out.set_elem(0, 2, in[0][2]);
-    out.set_elem(1, 0, in[1][0]);
+    out.set_elem(0, 1, in[1][0]);
+    out.set_elem(0, 2, in[2][0]);
+    out.set_elem(1, 0, in[0][1]);
     out.set_elem(1, 1, in[1][1]);
-    out.set_elem(1, 2, in[1][2]);
-    out.set_elem(2, 0, in[2][0]);
-    out.set_elem(2, 1, in[2][1]);
+    out.set_elem(1, 2, in[2][1]);
+    out.set_elem(2, 0, in[0][2]);
+    out.set_elem(2, 1, in[1][2]);
     out.set_elem(2, 2, in[2][2]);
-    out.vv.k[0] = in[0][3];
-    out.vv.k[1] = in[1][3];
-    out.vv.k[2] = in[2][3];
+
+    out.vv.k[0] = in[3][0];
+    out.vv.k[1] = in[3][1];
+    out.vv.k[2] = in[3][2];
 }
 
 inline void VxConvertMatrix(const VxMatrix &in, IVP_U_Matrix3 &out)
 {
+    // Transpose
     out.set_elem(0, 0, in[0][0]);
-    out.set_elem(0, 1, in[0][1]);
-    out.set_elem(0, 2, in[0][2]);
-    out.set_elem(1, 0, in[1][0]);
+    out.set_elem(0, 1, in[1][0]);
+    out.set_elem(0, 2, in[2][0]);
+    out.set_elem(1, 0, in[0][1]);
     out.set_elem(1, 1, in[1][1]);
-    out.set_elem(1, 2, in[1][2]);
-    out.set_elem(2, 0, in[2][0]);
-    out.set_elem(2, 1, in[2][1]);
+    out.set_elem(1, 2, in[2][1]);
+    out.set_elem(2, 0, in[0][2]);
+    out.set_elem(2, 1, in[1][2]);
     out.set_elem(2, 2, in[2][2]);
 }
 
